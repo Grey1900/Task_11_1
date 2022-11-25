f7fa128670ca259f0d271974f4a06ede48530f90
Sergey Plotnikov <splotnikov@splotnikov.com>
2022-11-25 19:43:35 +0400

    commit with edit of decrypt function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/******************************************************************************/
+/* File Name: Task_11_1                                                       */
+/* Data: 22.08.2022                                                           */
+/* Developer: Sergey Plotnikov                                                */
+/* Description: Solution for task 1 of Block 11 of the C++ study course       */
+/******************************************************************************/
+
 #include <iostream>
 #include <string>
 
@@ -5,9 +12,8 @@ std::string encryptCaesar(int, std::string);
 std::string decryptCaesar(int, std::string);
 
 int main()
-{
+{   
     int key;
-
     std::cout << "Enter the key: ";
     std::cin >> key;
     std::cout << "Enter the text: ";
@@ -15,23 +21,23 @@ int main()
     std::string str;
     std::getline(std::cin, str);
 
-    encryptCaesar(key, str);
-    std::string encryptString = encryptCaesar(encryptString);
-    decryptCaesar(key, encryptString);
+    std::cout << "Encrypted text: " << encryptCaesar(key, str) << std::endl;
+    std::cout << "Decrypted text: " << decryptCaesar(key, encryptCaesar(key, str)) << std::endl;
 }
 
 std::string encryptCaesar(int key, std::string str)
 {
-    std::string encryptString;
-    for (auto &c : str)
+  
+    for (auto& c : str)
     {
-        if (c >= 'a' && c <= 'z')
+        if (c >= 'a' && c <= 'z') {
             c = ((c - 'a' + key) % 26) + 'a';
-        else if (c >= 'A' && c <= 'Z')
+        }
+        else if (c >= 'A' && c <= 'Z') {
             c = ((c - 'A' + key) % 26) + 'A';
+        }
     }
-    encryptString = str;
-    return encryptString;
+    return str;
 }
 
 std::string decryptCaesar(int key, std::string encryptString)
